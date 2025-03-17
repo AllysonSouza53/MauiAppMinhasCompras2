6d7a88a93ca8610d6f6ac7aa1842fca4d0d4dd67
231026 <231026@CPD03-M13>
2025-03-17 10:54:04 -0300

    Inicio Aula 2 - 17/03

--- MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelper.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using MauiAppMinhasCompras.Models;
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQLiteDataBaseHelper
+    public class SQLiteDataBaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+        public SQLiteDataBaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> inserir(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Editar(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=? , Quantidade=?. Preco=? WHERE Id=?";
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.preco, p.Id);
+        }
+
+        public void Deletar(int id)
+        {
+
+        }
+
+        public void ColetarTodos()
+        {
+
+        }
+
+        public void Pesquisar(string q)
+        {
+
+        }
     }
 }
